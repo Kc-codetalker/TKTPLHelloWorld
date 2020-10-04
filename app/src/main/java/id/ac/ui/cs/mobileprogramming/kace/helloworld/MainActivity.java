3a03e7b96537cc0b2a96edfc2040c7f9c62ef0f4
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-10-04 22:43:02 +0700

    Add handler for overriding back button; showing a toast and confirmation dialog.

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.mobileprogramming.kace.helloworld;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -7,14 +8,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements BackButtonOverrideDialog.BackButtonOverrideDialogListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,11 +65,52 @@ public class MainActivity extends AppCompatActivity {
         Log.d("Button Pressed", "This is ENTER button.");
 
         // Capture the layout's EditText and get its text
-        EditText editText = (EditText) findViewById(R.id.editText2);
+        EditText editText = findViewById(R.id.editText2);
         String message = editText.getText().toString();
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText("Hello " + message + "!");
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d("Hardware Interact", "onBackPressed Called");
+//        Intent setIntent = new Intent(Intent.ACTION_MAIN);
+//        setIntent.addCategory(Intent.CATEGORY_HOME);
+//        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        startActivity(setIntent);
+
+        showNoticeToast(null);
+
+        showNoticeDialog();
+    }
+
+    public void showNoticeToast(String msg) {
+        Context context = getApplicationContext();
+        CharSequence text = (msg != null) ? msg: "This is toast! Can't go back.";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void showNoticeDialog() {
+        // Create an instance of the dialog fragment and show it
+        DialogFragment dialog = new BackButtonOverrideDialog();
+        dialog.show(getSupportFragmentManager(), "BackButtonOverrideDialogFragment");
+    }
+
+    // The dialog fragment receives a reference to this Activity through the
+    // Fragment.onAttach() callback, which it uses to call the following methods
+    // defined by the NoticeDialogFragment.NoticeDialogListener interface
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        Log.d("Tidak jadi back", "Yeay tidak jadi back!!");
     }
 }
