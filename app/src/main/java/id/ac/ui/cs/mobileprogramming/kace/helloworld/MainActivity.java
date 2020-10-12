e1d178fe664b4f128ced5391048da032ec36f560
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-10-12 19:36:11 +0700

    Changed snackbar text.

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/helloworld/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "This is a snackbar", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Choose a question.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
