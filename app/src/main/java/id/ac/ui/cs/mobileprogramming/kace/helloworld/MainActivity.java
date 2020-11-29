b2cb27705ffdd86f555a11c41ec6bdb2554fa365
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-11-29 22:23:58 +0700

    Send WAP name list to external service using Volley.

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/helloworld/MainActivity.java
@@ -20,10 +20,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -52,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.getWifis().observe(this, wifis -> {
             List<String> names = getWifiNamesFromResults(wifis);
+            sendWAPsRequestToService(names);
             ArrayAdapter adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,names);
             ListView listView = findViewById(R.id.wifiList);
             listView.setAdapter(adapter);
@@ -146,5 +156,34 @@ public class MainActivity extends AppCompatActivity {
         }
         Log.d("WIFI_SCAN_RESULT", "Wi-Fi name list size: " + viewModel.getWifis().getValue().size());
         return names;
+    }
+
+    private void sendWAPsRequestToService(List<String> names) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // See sent request at https://webhook.site/#!/852d65c3-2532-4181-8809-4e1ed6f7c0d1/95c04be6-a332-4d14-9fa6-80dff6a1e7bb/1
+        String url ="https://webhook.site/852d65c3-2532-4181-8809-4e1ed6f7c0d1";
+
+        HashMap<String, List<String>> params = new HashMap<>();
+        params.put("wifi_access_points", names);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("WIFI_SCAN_RESPONSE", "Wi-Fi APs request response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("WIFI_SCAN_RESPONSE", "Wi-Fi APs request error response: " + error.toString());
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
     }
 }
